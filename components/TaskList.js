52c4bc92c341bc5169626a9ff9816ac1582ac79c
lucas-rodrigues-dev <lucas.rodmendes@gmail.com>
2024-09-19 19:03:34 -0300

    update-focalpoint-app

--- components/TaskList.js
+++ components/TaskList.js
@@ -3,7 +3,7 @@ import styles from '../styles/TaskList.module.scss';
 export default function TaskList({ tasks, onDelete }) {
   return (
     <div className={styles.taskList}>
-      <h2>Suas tarefas de hoje</h2>
+      <h2 className={styles.taskTitle}>Suas tarefas de hoje</h2>
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
